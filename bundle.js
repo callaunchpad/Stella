741104746594e165c0cfd8b69664e586da8cde4d
Felix Su <felixs8696@berkeley.edu>
2016-12-25 21:32:07 -0500

    fixed undefined error by moving bundled content to a different file

--- bundle.js
+++ bundle.js
@@ -1,30 +1,4 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
-var $ = require('jquery')
-console.log('hi')
-
-var debug = true;
-function renderStatus(statusText) { document.getElementById('status').textContent = statusText; }
-function log(obj) { if (debug) console.log(obj); }
-
-var info = $("#info")[0];
-var info_start = $("#info_start")[0];
-var info_speak_now = $("#info_speak_now")[0];
-var info_no_speech = $("#info_no_speech")[0];
-var info_no_microphone = $("#info_no_microphone")[0];
-var info_allow = $("#info_allow")[0];
-var info_denied = $("#info_denied")[0];
-var info_blocked = $("#info_blocked")[0];
-var info_upgrade = $("#info_upgrade")[0];
-var start_button = $("#start_button")[0];
-var start_img = $("#start_img")[0];
-var results = $("#results")[0];
-var final_span = $("#final_span")[0];
-var interim_span = $("#interim_span")[0];
-var div_language = $("#div_language")[0];
-var select_language = $("#select_language")[0];
-var select_dialect = $("#select_dialect")[0];
-
-},{"jquery":2}],2:[function(require,module,exports){
 /*!
  * jQuery JavaScript Library v3.1.1
  * https://jquery.com/
@@ -10246,4 +10220,7 @@ if ( !noGlobal ) {
 return jQuery;
 } );
 
-},{}]},{},[1]);
+},{}],2:[function(require,module,exports){
+var $ = require('jquery');
+
+},{"jquery":1}]},{},[2]);
--- index.js
+++ index.js
@@ -1,24 +1,6 @@
-var $ = require('jquery')
-console.log('hi')
-
 var debug = true;
-function renderStatus(statusText) { document.getElementById('status').textContent = statusText; }
+function renderStatus(statusText) {
+  document.getElementById("status").textContent = statusText;
+  console.log(statusText);
+}
 function log(obj) { if (debug) console.log(obj); }
-
-var info = $("#info")[0];
-var info_start = $("#info_start")[0];
-var info_speak_now = $("#info_speak_now")[0];
-var info_no_speech = $("#info_no_speech")[0];
-var info_no_microphone = $("#info_no_microphone")[0];
-var info_allow = $("#info_allow")[0];
-var info_denied = $("#info_denied")[0];
-var info_blocked = $("#info_blocked")[0];
-var info_upgrade = $("#info_upgrade")[0];
-var start_button = $("#start_button")[0];
-var start_img = $("#start_img")[0];
-var results = $("#results")[0];
-var final_span = $("#final_span")[0];
-var interim_span = $("#interim_span")[0];
-var div_language = $("#div_language")[0];
-var select_language = $("#select_language")[0];
-var select_dialect = $("#select_dialect")[0];
