6301396ba3dcd3b7525c804c7fd4e83ee9620e1a
PeterTheDestroyer <peter.lee@berkeley.edu>
2017-01-05 21:44:52 -0600

    Fixed app id bug

--- background/background.js
+++ background/background.js
@@ -3,7 +3,7 @@ chrome.browserAction.onClicked.addListener(function(activeTab) {
   chrome.tabs.query(query, function(tabs) {
     if (tabs.length == 0) {
       // var windowSpecs = {width: 620, height: 430, type: 'normal', state: 'normal', focused: true, url: APP_URL};
-      var tabSpecs = {active: true, url: "chrome-extension://fkejanlfomiednfigechphohcjaklelo/index.html"};
+      var tabSpecs = {active: true, url: "chrome-extension://" + chrome.runtime.id + "/index.html"};
       chrome.tabs.create(tabSpecs, function(tab) {
         console.log("Vocal Web site tab initialized: " + tab.url);
       });
