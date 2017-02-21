b9c26e6ec8e173642064deae0a3b9d75ee28cd66
Hank O'Brien <hank.obrien900@icloud.com>
2017-02-21 14:59:12 -0800

    few changes before this file is ignored

--- npm/bundle.js
+++ npm/bundle.js
@@ -20764,9 +20764,11 @@ var test = "stemmer test x";
 Debug.log(test.tokenizeAndStem());
 Debug.log("stemmer".stem());
 
-var helpWords = ["help", "assist", "documentation"];
+var helpWords = ["HLP", "ASST", "TKMNTXN"];
 
-function parseCommand(final_transcript) {
+
+
+function shouldOpenHelp(final_transcript) {
     Debug.log(final_transcript);
 
     // var tokenList = final_transcript.tokenizeAndStem();
@@ -20778,28 +20780,59 @@ function parseCommand(final_transcript) {
 
     var metaphone = natural.Metaphone;
 
-    function processWrapper(curr, index, arr) {
+    function toPhoneme(curr, index, arr) {
         arr[index] = metaphone.process(curr);
     }
-    helpWords.forEach(processWrapper);
-    tokenList.forEach(processWrapper);
-    Debug.log("****");
+    // helpWords.forEach(toPhoneme);
+    tokenList.forEach(toPhoneme);
+    // helpWords = [].concat.apply([], helpWords); //flattens 2D arrays DoubleMetaphone generates
+    tokenList = [].concat.apply([], tokenList);
+
+    // var helpPhonemes = [];
+    // for(var i = 0; i < helpWords.length; i++){
+    //     helpPhonemes.concat(metaphone.process(helpWords[i]));
+    // }
+    //
+    // var tokenPhonemes = [];
+    // for(var j = 0; i < tokenList.length; i++){
+    //     tokenPhonemes.concat(metaphone.process(tokenList[j]));
+    // }
+
+
+    Debug.log("help phonemes");
+    Debug.log(helpWords);
+    Debug.log("token phonemes");
     Debug.log(tokenList);
 
     var NGrams = natural.NGrams;
     //number of words per ngram  = 2
     var nGramSet = NGrams.ngrams(tokenList.join(" "), 2);
 
-    Debug.log(nGramSet);
+    var sortedTokens = tokenList.slice().sort();
+    helpWords.sort();
 
-    Debug.log(NGrams.ngrams(helpWords.join(" "), 2));
+    Debug.log(sortedTokens);
+    Debug.log(helpWords);
 
+    return hasSimilarity(sortedTokens, helpWords);
 
 
 }
 
+function hasSimilarity(sortedWords, helpWords){
+    for(var i = 0; i < helpWords.length; i++){
+        if(sortedWords.indexOf(helpWords[i]) > -1){
+            Debug.log("There was a similarity");
+            return true;
+        }
+    }
+    Debug.log("No similarity");
+    return false;
+}
+
+
 module.exports = {
-    parseCommand: parseCommand
+    shouldOpenHelp: shouldOpenHelp
 };
 },{"natural":39}],131:[function(require,module,exports){
 
