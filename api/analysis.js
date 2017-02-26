34acfd61d7d93d4ab3a152d32c60c4b2c7166518
Arsh Zahed <arshzahed@gmail.com>
2017-02-25 17:40:09 -0800

    forgto to change TRIGGER_NAME back to Stella. Fixed now

--- api/analysis.js
+++ api/analysis.js
@@ -22,7 +22,7 @@ function takeAction(text) {
     var action = natural.otherClassifier.classify(tokens);
     action = natural.determineScroll(action, text, tokens);
     console.log('action is : '+action);
-    natural.functionMap[action]();
+    natural.functionMap[action](text.replace(TRIGGER_NAME, '').trimLeft().trimRight());
   }
 }
 
