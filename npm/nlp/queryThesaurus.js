856693b8ffc741f85615a081fc7f13b6f068d371
Hank O'Brien <hank.obrien900@icloud.com>
2017-03-30 15:52:54 -0700

    small things

--- npm/nlp/queryThesaurus.js
+++ npm/nlp/queryThesaurus.js
@@ -1,3 +1,18 @@
 /**
  * Created by Hank on 3/25/17.
  */
+var AWS = require('aws-sdk');
+AWS.config.loadFromPath('./config.json');
+s3 = new AWS.s3({apiVersion : '2006-03-01'});
+
+var bucketParams = {
+    Bucket : process.argv[2]
+};
+
+s3.createBucket(bucketParams, function (err, data){
+    if(err){
+        Debug.log(err);
+    } else {
+        Debug.log("Success", data.location);
+    }
+});
