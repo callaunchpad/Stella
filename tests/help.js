bd2b48226dd9c26ebc631c43ef1a004eca6c5283
Arsh Zahed <arshzahed@gmail.com>
2017-04-08 14:25:49 -0700

    added help.js

--- tests/help.js
+++ tests/help.js
@@ -2,7 +2,7 @@ module.exports = {
   'Help Menu test': function (client) {
     client
       .url('')
-      .setValue('input[id="message_input"]', 'foo@bar.com')
+      .setValue('input[id="message_input"]', 'open help menu')
       .click('button[type="submit"]')
       .assert.containsText('main', 'News feed')
       .end();
