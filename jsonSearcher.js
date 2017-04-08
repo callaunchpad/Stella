1803d0c0f10dadc6498f322bb66ee2e6d65f1a6e
Hank O'Brien <hank.obrien900@icloud.com>
2017-04-08 12:41:01 -0700

    fixed jsonSearcher.js file

--- jsonSearcher.js
+++ jsonSearcher.js
@@ -1,4 +1,4 @@
-var raw_thesaurus = JSON.parse('jsonThesaurus.json')
+var raw_thesaurus = JSON.parse('jsonThesaurus.json');
 var bs = require('binarysearch');
 
 
@@ -8,8 +8,8 @@ var thesaurus = jQuery.map(raw_thesaurus, function(value, index){
 
 //look at 2 values to make sure boundaries between
 //entries are read correctly
-Debug.log('thesaurus data 0' + thesaurus[0])
-Debug.log('thesaurus data 1' + thesaurus[1])
+Debug.log('thesaurus data 0' + thesaurus[0]);
+Debug.log('thesaurus data 1' + thesaurus[1]);
 
 // raw_thesaurus = JSON.parse('jsonThesaurus');
 
@@ -19,9 +19,9 @@ Debug.log('thesaurus data 1' + thesaurus[1])
 @param[key]: the string to search for in the thesaurus
 @returns[]: redirects to binary search
 **/
-function finder(var thesaurus, var key){
-  small = 0;
-  big = thesaurus.length - 1;
+function finder(thesaurus, key){
+  var small = 0;
+  var big = thesaurus.length - 1;
   return binaryDictSearch(thesaurus, key, small, big);
 }
 
@@ -33,10 +33,10 @@ function finder(var thesaurus, var key){
 @param[big]: upper bound index for binary search
 @returns[json/string]: if the thesaurus entry matching key is found, return the json entry else return "not found"
 **/
-function binaryDictSearch(var thesaurus, var key, var small, var big){
+function binaryDictSearch(thesaurus, key, small, big){
 
   while(small <= big){
-    middle = (small + big)/2
+    var middle = (small + big)/2;
 		if(thesaurus[middle]['word'] === key){
 			return thesaurus[middle]
     }	else if(thesaurus[middle]['word'] > key){
@@ -54,13 +54,17 @@ function binaryDictSearch(var thesaurus, var key, var small, var big){
 @param[bayesBank]: a bank of words that are currently recognized by the bayes classifer
 @returns[string]: returns a synonym found in the bayesBank else "not found"
 **/
-function findSynonym(var jsonEntry, var bayesBank){
-  synonyms = jsonEntry['synonyms']
-	bank = set(bayesBank)
-	for s in synonyms:
-		if s in bank:
-			return s
-	return "not found"
+function findSynonym(jsonEntry, bayesBank){
+    var synonyms = jsonEntry['synonyms'];
+    var bank = set(bayesBank);
+    for(var i = 0; i < synonyms.length; i++) {
+        var s = synonyms[i];
+        if (s in bank) {
+            return s;
+        }
+    }
+    return "not found";
+
 }
 
 module.exports = {findSynonym : findSynonym};
