c090ab0f08a34b79cea97d782fdf4e5ae58babe2
Hank O'Brien <hank.obrien900@icloud.com>
2017-04-08 21:49:59 -0700

    made new thesaurus with better format

--- jsonSearcher.js
+++ jsonSearcher.js
@@ -1,5 +1,17 @@
-var raw_thesaurus = JSON.parse('jsonThesaurus.json');
+// var raw_thesaurus = JSON.parse('jsonThesaurus');
 // var bs = require('binarysearch');
+
+// function readThesaurus(){
+//     var rawFile = new XMLHttpRequest();
+//     rawFile.overrideMimeType("application/json");
+//     rawFile.open("GET", file, true);
+//     rawFile.onreadystatechange = function() {
+//         if (rawFile.readyState === 4 && rawFile.status == "200") {
+//             callback(rawFile.responseText);
+//         }
+//     }
+//     rawFile.send(null);
+// }
 
 
 var thesaurus = jQuery.map(raw_thesaurus, function(value, index){
